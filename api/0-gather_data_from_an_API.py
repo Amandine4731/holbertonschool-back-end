12366113df8ca3c5e111d3c82dc75fb557f09bd7
Amandine4731 <5618@holbertonstudents.com>
2023-04-07 02:19:57 -0700

    pep 8 79 caracters max

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -15,9 +15,11 @@ if __name__ == '__main__':
 
     # retrieve all databases
     # the required first parameter of the 'get' method is the 'url'
-    api_url_users = requests.get(f"https://jsonplaceholder.typicode.com/users/{unique_id}").json()
+    api_url_users = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{unique_id}").json()
 
-    api_url_todos = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
+    api_url_todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos/").json()
 
     # I created all the variables that I need
     # Initialize variables to count
@@ -25,14 +27,17 @@ if __name__ == '__main__':
     TOTAL_NUMBER_OF_TASKS = 0
     EMPLOYEE_NAME = api_url_users['name']
 
-    # Loop that calculates the number of tasks to do and tasks performed by the same user/employee
-    for data in api_url_todos:
-        if data['userId'] == unique_id:
-            TOTAL_NUMBER_OF_TASKS += 1  # Count all tasks TO DO for the same user user/employee
-            if data['completed']:
-                NUMBER_OF_DONE_TASKS += 1  # Count all tasks REALISED for the same user user/employee
+    # Loop to calculate the number of tasksto do and to do and tasks performed
+    for key in api_url_todos:
+        if key['userId'] == unique_id:
+            # Count all tasks TO DO for the same user user/employee
+            TOTAL_NUMBER_OF_TASKS += 1
+            if key['completed']:
+                # Count all tasks REALISED for the same user user/employee
+                NUMBER_OF_DONE_TASKS += 1
     # Create the program : Here, just print sentence with retrieved variables
-    print(f"Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    print(f"Employee {EMPLOYEE_NAME} is done with "
+          f"tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
 
     # Loop that retrieves title of done tasks
     for data in api_url_todos:
