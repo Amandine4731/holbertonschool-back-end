b07520594bb6edb6e94cc43cf4b98aee8b8d6319
Amandine4731 <5618@holbertonstudents.com>
2023-04-07 07:33:13 -0700

    query in the url

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -12,26 +12,26 @@ from sys import argv
 if __name__ == '__main__':
     """ to execute this program - entry point """
 
+    unique_id = int(argv[1])
     # retrieve the database
     api_url_todos = requests.get(
-        "https://jsonplaceholder.typicode.com/todos/").json()
-
-    USER_ID = int(argv[1])
-
+        f"https://jsonplaceholder.typicode.com/todos/?userId={unique_id}").json()
     # retrieve database of all users
     # the required first parameter of the 'get' method is the 'url'
     api_url_users = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{USER_ID}").json()
+        f"https://jsonplaceholder.typicode.com/users/{unique_id}").json()
+
+    USER_ID = api_url_users['id']
 
     with open(f"{USER_ID}.csv", 'wt') as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
 
         for key in api_url_todos:
-            new_list = []
-            new_list.append(key['userId'])
-            new_list.append(api_url_users['username'])
-            new_list.append(key['completed'])
-            new_list.append(key['title'])
-
+            if key['userId'] == unique_id:
+                new_list = []
+                new_list.append(USER_ID)
+                new_list.append(api_url_users['username'])
+                new_list.append(key['completed'])
+                new_list.append(key['title'])
             writer.writerow(new_list)
         f.close()
